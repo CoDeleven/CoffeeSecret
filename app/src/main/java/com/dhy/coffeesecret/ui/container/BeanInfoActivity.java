77501bf29f541b66c62bfd162eb2d8c6ff5e0a3f
SoyaLeaf <597231656@qq.com>
2017-02-08 19:57:15 +0800

    我的货柜 豆种详情部分界面

---
+++ app/src/main/java/com/dhy/coffeesecret/ui/container/BeanInfoActivity.java
@@ -0,0 +1,35 @@
+package com.dhy.coffeesecret.ui.container;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+import com.dhy.coffeesecret.R;
+
+public class BeanInfoActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bean_info);
+        mToolbar = (Toolbar) findViewById(R.id.toolBar);
+
+        setSupportActionBar(mToolbar);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_beaninfo,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
